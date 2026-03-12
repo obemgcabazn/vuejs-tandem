1bfb4f5d078c18474337a047c7c370d1feeac51a
MAKSIM <max_air@bk.ru>
2026-03-12 22:32:05 +0300

    feat: add levels for cab zone

--- src/open AI Requests/openrouter.request.ts
+++ src/open AI Requests/openrouter.request.ts
@@ -21,7 +21,7 @@ export async function openrouterRequest(message: string) {
     ],
   })
 
-  return completion?.choices[0].message.content
+  return completion?.choices[0]?.message?.content
 }
 
 export function textRequest(question: string, userMessage: string) {
